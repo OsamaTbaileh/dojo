ee07008299987926da0ec6872f7f921a635af86b
Osama Tbaileh <osama.tb02@gmail.com>
2022-08-24 09:10:14 +0300

    update

---
+++ 6/Tbaileh_Osama_JS_Tinkering.js
@@ -0,0 +1,11 @@
+var ChildHeight =1;
+function displayIfChildIsAbleToRideTheRollerCoaster(ChildHeight){
+    if (ChildHeight>52){
+        console.log("Get on that ride, kiddo!");
+    }
+        else{
+            console.log("Sorry kiddo. Maybe next year");
+        }
+    
+}
+displayIfChildIsAbleToRideTheRollerCoaster();
